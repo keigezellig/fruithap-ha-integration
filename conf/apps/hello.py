25381524dd2ebe3798c9a4f188eea46e87a1dcc8
keigezellig <mh.joosten666@gmail.com>
2017-11-02 10:50:16 +0100

    Added simple event trigger that posts to MQ

--- conf/apps/hello.py
+++ conf/apps/hello.py
@@ -9,5 +9,21 @@ import appdaemon.appapi as appapi
 class HelloWorld(appapi.AppDaemon):
 
   def initialize(self):
+     self.listen_state(self.on_switch_change, "switch") 
+     self.listen_state(self.on_light_change, "light") 
      self.log("Hello from AppDaemon")
      self.log("You are now ready to run Apps!")
+     
+  def on_switch_change(self, entity, attribute, old, new, kwargs):
+      device, entity_name = self.split_entity(entity)
+      msg = device + " "+entity_name + " " +old + " "+new
+      self.log(msg)
+      self.call_service("mqtt/publish", topic = "events", payload = msg)
+      
+  def on_light_change(self, entity, attribute, old, new, kwargs):
+      device, entity_name = self.split_entity(entity)
+      msg = device + " "+entity_name + " " +old + " "+new
+      self.log(msg)
+      self.call_service("mqtt/publish", topic = "events", payload = msg)
+      
+      
